a90eea5a26166ddaf73e5c8fcb1e7011fe9fcbf1
spk-cpsamut <taresupakon@gmail.com>
2020-06-06 07:56:29 +0700

    9

---
+++ 6.simple-string.dart
@@ -0,0 +1,14 @@
+var s1 = 'Single quotes work well for string literals.';
+var s2 = "Double quotes work just as well.";
+var s3 = 'It\'s easy to escape the string delimiter.'; // ใช้ \ ในการ escape
+var s4 = "It's even easier to use the other delimiter.";
+var s = r'In a raw string, not even \n gets special treatment.';
+
+var age = 35;
+var str = 'My age is: $age';
+
+var s5 = ''' 
+ 
+ hello
+
+''';
