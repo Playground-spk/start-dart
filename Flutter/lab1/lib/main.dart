2c13de972bd775df957c3edbc7b2d6a76ec117ba
spk-cpsamut <taresupakon@gmail.com>
2020-07-04 18:29:29 +0700

    try stateful widget

---
+++ Flutter/lab1/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// build screen ui mobile with scaffold
+
+// children in widget  is mean can have many widget
+
+// child in widget is mean can have one widget
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  int _quiestionIndex = 0;
+
+  void _onPress() {
+    print('hello');
+    setState(() {
+      _quiestionIndex = (_quiestionIndex + 1) % 2;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var question = ['who is your daddy', 'who is your mom'];
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('My First Apps'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text(question[_quiestionIndex]),
+          RaisedButton(
+            child: Text('Justin'),
+            onPressed: () => {print('justin')},
+          ),
+          RaisedButton(
+            onPressed: _onPress,
+            child: Text('use out source function'),
+          ),
+          Text('hello'),
+        ],
+      ),
+    ));
+  }
+}
