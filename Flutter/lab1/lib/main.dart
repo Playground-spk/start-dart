bb7d163a254c0ecb4d9e01b53d73f8ec1ea3ecde
spk-cpsamut <taresupakon@gmail.com>
2020-07-05 14:57:19 +0700

    lesson mainAxis&crossAxis in GRID

--- Flutter/lab1/lib/main.dart
+++ Flutter/lab1/lib/main.dart
@@ -1,70 +1,60 @@
 import 'package:flutter/material.dart';
 
-import 'question.dart';
+void main() => runApp(MyApp());
 
-void main() {
-  runApp(MyApp());
-}
-
-// build screen ui mobile with scaffold
-
-// children in widget  is mean can have many widget
-
-// child in widget is mean can have one widget
-
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  int _quiestionIndex = 0;
-
-  void _onPress(name) {
-    if (_quiestionIndex == 0) {
-      if (name == 'toong') {
-        print('ถูกต้อง');
-      } else
-        print('ผิด');
-    }
-    if (_quiestionIndex == 1) {
-      if (name == 'Lek') {
-        print('ถูกต้อง');
-      } else
-        print('ผิด');
-    }
-
-    setState(() {
-      _quiestionIndex = (_quiestionIndex + 1) % 2;
-    });
-  }
-
-  @override
+class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
-    var question = ['who is your daddy', 'who is your mom'];
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text('My First Apps'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Question(question[_quiestionIndex]),
-          RaisedButton(
-            child: Text('toong'),
-            onPressed: () => _onPress('toong'),
-          ),
-          Container(
-            margin: EdgeInsets.all(20),
-            child: RaisedButton(
-              onPressed: () => _onPress('Lek'),
-              child: Text('Lek'),
+      title: 'My App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('My test'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                Card(
+                  color: Colors.deepOrange,
+                  child: Text('List of Row'),
+                ),
+                Card(
+                  color: Colors.deepOrange,
+                  child: Text('List of Row'),
+                ),
+                Card(
+                  color: Colors.deepOrange,
+                  child: Text('List of Row'),
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
-    ));
+            Card(
+              color: Colors.deepOrange,
+              child: Text('List of Col'),
+            ),
+            Card(
+              color: Colors.deepOrange,
+              child: Text('List of Col'),
+            ),
+            Card(
+              color: Colors.deepOrange,
+              child: Text('List of Col'),
+            ),
+            Card(
+              color: Colors.deepOrange,
+              child: Text('List of Col'),
+            )
+          ],
+        ));
   }
 }
